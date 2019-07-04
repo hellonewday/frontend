637d2d7196a27f69f04b62b3a447f9c243ebd510
Your Name <you@example.com>
2019-07-04 11:42:12 +0700

    Updates

---
+++ precache-manifest.a86e517ae3122bfc0cdd7bd262bc8bd6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5db85c663bab291a5f99d6f0f57e3a1",
+    "url": "/frontend/index.html"
+  },
+  {
+    "revision": "3a184836bf46e047d7e9",
+    "url": "/frontend/static/css/main.98d65c9f.chunk.css"
+  },
+  {
+    "revision": "161f74108b685fe5c908",
+    "url": "/frontend/static/js/2.569e4c42.chunk.js"
+  },
+  {
+    "revision": "3a184836bf46e047d7e9",
+    "url": "/frontend/static/js/main.0b4842c8.chunk.js"
+  },
+  {
+    "revision": "dbd924a9862891b98c97",
+    "url": "/frontend/static/js/runtime~main.f332d61a.js"
+  },
+  {
+    "revision": "2f783c845982ad84b36279387d21ade6",
+    "url": "/frontend/static/media/carousel1.2f783c84.jpg"
+  },
+  {
+    "revision": "ef5cc1bca4dd2ec24c3c13f44122205d",
+    "url": "/frontend/static/media/home2.ef5cc1bc.jpg"
+  },
+  {
+    "revision": "346b2a4543e6a117e532be04f4b87ff4",
+    "url": "/frontend/static/media/logo-brand.346b2a45.png"
+  },
+  {
+    "revision": "3cd9fab38768f73e526e8b9f9bd6e3f6",
+    "url": "/frontend/static/media/register-background.3cd9fab3.jpg"
+  }
+]);
